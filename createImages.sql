85cfc36db42e475df5af776358c1f1f4b6020be5
alexamos32 <alexanderamos@me.com>
2020-02-21 00:22:07 -0400

    Creating the RAML Documentation

--- createImages.sql
+++ createImages.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS images;
 CREATE TABLE images (
   imageId    INT             NOT NULL         AUTO_INCREMENT,
-  imageName  varchar(255)    DEFAULT NULL,
+  description  varchar(255)    DEFAULT NULL,
   path       varchar(255)    NOT NULL,   
   owner      INT             NOT NULL         REFERENCES users(userId),
   PRIMARY KEY (imageId)
