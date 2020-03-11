6701fecdc097232471904632252c370632fcf422
Alex Amos <alexanderamos@me.com>
2020-03-11 11:15:36 -0300

    File moved

--- insertUser.sql
+++
@@ -1,8 +0,0 @@
-DELIMITER //
-DROP PROCEDURE IF EXISTS insertUser //
-
-CREATE PROCEDURE insertUser(IN p_username VARCHAR(50))
-BEGIN
-    INSERT INTO users(username) VALUES (p_username);
-END //
-DELIMITER ;
