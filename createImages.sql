086b6147863b5fe4d34e40c54019e56aaa67da97
Alex Amos <alexanderamos@me.com>
2020-03-11 11:14:07 -0300

    File moved

--- createImages.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS images;
-CREATE TABLE images (
-  imageId    INT             NOT NULL         AUTO_INCREMENT,
-  description  varchar(255)    DEFAULT NULL,
-  path       varchar(255)    NOT NULL,   
-  owner      INT             NOT NULL         REFERENCES users(userId),
-  PRIMARY KEY (imageId)
-);
