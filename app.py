e3cd58aa0525ccc79595b2a35880d6e71beaea76
brmarco <bmarco@unb.ca>
2020-03-11 23:57:11 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify, abort, request, make_response
+import os
+from flask import Flask, jsonify, abort, request, make_response, session, render_template
 from flask_restful import Resource, Api, reqparse
 from flask_session import Session
-import pymysql.cursors
 import json
+from ldap3 import Server, Connection, ALL
+from ldap3.core.exceptions import *
+import pymysql.cursors
+
 
 import cgitb
 import cgi
@@ -11,9 +15,14 @@ import sys
 cgitb.enable()
 
 import settings
-
+UPLOAD_FOLDER ='/path/to/folder' #PLACEHOLDER
+ALLOWED_EXTENSIONS = set(['png','jpg','jpeg'])
 app = Flask(__name__)
-api= Api(app)
+app.secret_key = settings.SECRET_KEY
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_COOKIE_NAME'] = 'peanutButter'
+app.config['SESSION_COOKIE_DOMAIN'] = settings.APP_HOST
+Session(app)
 
 
 ####################################################################################
@@ -39,13 +48,83 @@ class Root(Resource):
     def get(self):
         return make_response( jsonify({ "message": "Welcome to the Image Store Repo. Go to /signin POST to sign in."}), 200)
 
-api.add_resource(Root,'/') 
 
 
-class Signin(Resource):
-    def post(self):
-        return
-        #TODO: FINISH SIGNING
+class SignIn(Resource):
+	#
+	# Login, start a session and set/return a session cookie
+	#
+	# Example curl command:
+	# curl -i -H "Content-Type: application/json" -X POST -d '{"username": "Casper", "password": "cr*ap"}'
+	#  	-c cookie-jar http://info3103.cs.unb.ca:61340/signin
+	#
+	def post(self):
+		if not request.json:
+			abort(400) # bad request
+		# Parse the json
+		parser = reqparse.RequestParser()
+		try:
+			# Check for required attributes in json document, create a dictionary
+			parser.add_argument('username', type=str, required=True)
+			parser.add_argument('password', type=str, required=True)
+			request_params = parser.parse_args()
+		except:
+			abort(400) # bad request
+
+		# Already logged in
+		if request_params['username'] in session:
+			response = {'status': 'success'}
+			responseCode = 200
+		else:
+			try:
+				ldapServer = Server(host=settings.LDAP_HOST)
+				ldapConnection = Connection(ldapServer,
+					raise_exceptions=True,
+					user='uid='+request_params['username']+', ou=People,ou=fcs,o=unb',
+					password = request_params['password'])
+				ldapConnection.open()
+				ldapConnection.start_tls()
+				ldapConnection.bind()
+				# At this point we have sucessfully authenticated.
+				session['username'] = request_params['username']
+				response = {'status': 'success' }
+				responseCode = 201
+			except (LDAPException, error_message):
+				response = {'status': 'Access denied'}
+				responseCode = 403
+			finally:
+				ldapConnection.unbind()
+
+		return make_response(jsonify(response), responseCode)
+
+	# GET: Check for a login
+	#
+	# Example curl command:
+	# curl -i -H "Content-Type: application/json" -X GET -b cookie-jar
+	#	http://info3103.cs.unb.ca:61340/signin
+	def get(self):
+		if 'username' in session:
+			response = {'status': 'success'}
+			responseCode = 200
+		else:
+			response = {'status': 'fail'}
+			responseCode = 403
+
+		return make_response(jsonify(response), responseCode)
+
+	def delete(self):
+		###############
+		#Log Out a User
+		###############
+		if 'username' in session:
+			session.clear()
+			response = {'status': 'successfully logged out'}
+			responseCode = 200
+		else:
+			response = {'status': 'could not log out'}
+			responseCode = 403
+
+		return make_response(jsonify(response), responseCode)
 
 
 class Users(Resource):
@@ -86,7 +165,8 @@ class Users(Resource):
         uri = uri + str(request.url_rule) + '/' + str(row['LAST_INSERT_ID()'])
         return make_response(jsonify( { "usr" : uri } ), 201) 
 
-api.add_resource(Users, '/users')
+
+
 
 
 class User(Resource):
@@ -120,7 +200,7 @@ class User(Resource):
     def update(self, userId):
         return
 
-api.add_resource(User, '/users/<int:userId>')
+
 
 class Images(Resource):
     def get(self, userId):
@@ -163,6 +243,16 @@ class Images(Resource):
         parser = reqparse.RequestParser()
         #try:
         return "Hey"
-            
 
+
+#Create EndPoints            
+api= Api(app)
+api.add_resource(Users, '/users')
 api.add_resource(Images, '/users/<int:userId>/images')
+api.add_resource(User, '/users/<int:userId>')
+api.add_resource(SignIn, '/SignIn')
+api.add_resource(Root,'/') 
+
+if __name__ == "__main__":
+	app.run(host=settings.APP_HOST, port=settings.APP_PORT, debug=settings.APP_DEBUG)
+
