e17d4dc44b5bd9c633e86c5f31ae2bf1dcf9d751
alexamos32 <alexanderamos@me.com>
2020-03-12 00:03:12 -0300

    Working on image storage

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify, abort, request, make_response
+from flask import Flask, jsonify, abort, request, make_response, session
 from flask_restful import Resource, Api, reqparse
 from flask_session import Session
 import pymysql.cursors
 import json
+import werkzeug, os
+
+from ldap3 import Server, Connection, ALL
+from ldap3.core.exceptions import *
 
 import cgitb
 import cgi
@@ -15,17 +19,23 @@ import settings
 app = Flask(__name__)
 api= Api(app)
 
+# Setting up SESSION info
+app.secret_key = settings.SECRET_KEY
+app.config['SESSION_TYPE'] = ' wc wcstem'
+app.config['SESSION_COOKIE_NAME'] = 'peanutButter'
+app.config['SESSION_COOKIE_DOMAIN'] = settings.APP_HOST
+Session(app)
 
 ####################################################################################
 # Error handlers
 
 @app.errorhandler(400) # decorators to add to 400 response
 def not_found(error):
-	return make_response(jsonify( { "status": "Bad request" } ), 400)
+        return make_response(jsonify( { "status": "Bad request" } ), 400)
 
 @app.errorhandler(404) # decorators to add to 404 response
 def not_found(error):
-	return make_response(jsonify( { "status": "Resource not found" } ), 404)
+        return make_response(jsonify( { "status": "Resource not found" } ), 404)
 
 ####################################################################################
 
@@ -36,133 +46,197 @@ def not_found(error):
 
 #TODO: Create and return index.html
 class Root(Resource):
-    def get(self):
-        return make_response( jsonify({ "message": "Welcome to the Image Store Repo. Go to /signin POST to sign in."}), 200)
+        def get(self):
+                return make_response(jsonify({ "message": "Welcome to the Image Store Repo. Go to /signin POST to sign in."}), 200)
 
-api.add_resource(Root,'/') 
+api.add_resource(Root,'/')
 
 
 class Signin(Resource):
-    def post(self):
-        return
-        #TODO: FINISH SIGNING
+        # Login, start a session and set/return a session cookie
+        #
+        # Example curl command:
+        # curl -i -H "Content-Type: application/json" -X POST -d '{"username": "Casper", "password": "cr*ap"}'
+        #-c cookie-jar http://info3103.cs.unb.ca:61340/signin
+        #
+        def post(self):
+                if not request.json:
+                        abort(400) # bad request
+                # Parse the json
+                parser = reqparse.RequestParser()
+                try:
+                        # Check for required attributes in json document, create a dictionary
+                        parser.add_argument('username', type=str, required=True)
+                        parser.add_argument('password', type=str, required=True)
+                        request_params = parser.parse_args()
+                except:
+                        abort(400) # bad request
+
+                # Already logged in
+                if request_params['username'] in session:
+                        response = {'status': 'success'}
+                        responseCode = 200
+                else:
+                        try:
+                                ldapServer = Server(host=settings.LDAP_HOST)
+                                ldapConnection = Connection(ldapServer,
+                                        raise_exceptions=True,
+                                        user='uid='+request_params['username']+', ou=People,ou=fcs,o=unb',
+                                        password=request_params['password'])
+                                ldapConnection.open()
+                                ldapConnection.start_tls()
+                                ldapConnection.bind()
+                                # At this point we have sucessfully authenticated.
+                                session['username'] = request_params['username']
+                                response = {'status': 'success' }
+                                responseCode = 201
+                        except (LDAPException):
+                                response = {'status': 'Access denied'}
+                                responseCode = 403
+                        finally:
+                                ldapConnection.unbind()
+                return make_response(jsonify(response), responseCode)
+
+        #GET: Check for a login
+        #Example curl command:
+        # curl -i -H "Content-Type: application/json" -X GET -b cookie-jar
+        #http://info3103.cs.unb.ca:61340/signin
 
 
+        def get(self):
+                if 'username' in session:
+                        response = {'status': 'success'}
+                        responseCode = 200
+                else:
+                        response = {'status': 'fail'}
+                        responseCode = 403
+
+                return make_response(jsonify(response), responseCode)
+
+        # DELETE: Logout: remove session
+        #
+        # Example curl command:
+        # curl -i -H "Content-Type: application/json" -X DELETE -b cookie-jar
+        #       http://info3103.cs.unb.ca:61340/signin
+        #
+        #       Here's your chance to shine!
+        #
+api.add_resource(Signin,'/signin')
 class Users(Resource):
-    #ONLY POST
-    #For creating new users, but do not want to Allow GET on all users
-    def post(self):
-        # Sample command line usage:
+        #'ONLY POST
+        #For creating new users, but do not want to Allow GET on all users
+        def post(self):
+                # Sample command line usage:
         #
         # curl -i -X POST -H "Content-Type: application/json"
         #    -d '{"username": "johnsmith1"}'
         #         http://info3103.cs.unb.ca:xxxxx/schools
         
-        if not request.json or not 'username' in request.json:
-            abort(400)
+                if not request.json or not 'username' in request.json:
+                        abort(400)
 
-        username = request.json['username'] 
+                username = request.json['username']
 
-        try:
-            dbConnection = pymysql.connect(settings.DB_HOST,
-                settings.DB_USER,
-                settings.DB_PASSWD,
-                settings.DB_DATABASE,
-                charset='utf8mb4',
-                cursorclass= pymysql.cursors.DictCursor)
-            sql = 'insertUser'
-            cursor = dbConnection.cursor()
-            sqlArgs = (username)
-            cursor.callproc(sql,sqlArgs)
-            row = cursor.fetchone()
-            dbConnection.commit()
-        except:
-            abort(500) #Server error
-        finally:
-            cursor.close()
-            dbConnection.close()
-        #Construct URI to return to user for successful creation
-        uri = 'http://' + settings.APP_HOST + ':' + str(settings.APP_PORT)
-        uri = uri + str(request.url_rule) + '/' + str(row['LAST_INSERT_ID()'])
-        return make_response(jsonify( { "usr" : uri } ), 201) 
+                try:
+                        dbConnection = pymysql.connect(settings.DB_HOST,
+                                settings.DB_USER,
+                                settings.DB_PASSWD,
+                                settings.DB_DATABASE,
+                                charset='utf8mb4',
+                                cursorclass= pymysql.cursors.DictCursor)
+                        sql = 'insertUser'
+                        cursor = dbConnection.cursor()
+                        sqlArgs = (username)
+                        cursor.callproc(sql,sqlArgs)
+                        row = cursor.fetchone()
+                        dbConnection.commit()
+                except:
+                        abort(500) #Server error
+                finally:
+                        cursor.close()
+                        dbConnection.close()
+                #Construct URI to return to user for successful creation
+                uri = 'http://' + settings.APP_HOST + ':' + str(settings.APP_PORT)
+                uri = uri + str(request.url_rule) + '/' + str(row['LAST_INSERT_ID()'])
+                return make_response(jsonify( { "usr" : uri } ), 201)
 
 api.add_resource(Users, '/users')
 
 
 class User(Resource):
-    #GET: get info about user
-    #UPDATE: Update User info
-    def get(self, username):
-        try:
-            dbConnection = pymysql.connect(
-                settings.DB_HOST,
-                settings.DB_USER,
-                settings.DB_PASSWD,
-				settings.DB_DATABASE,
-                charset='utf8mb4',
-				cursorclass= pymysql.cursors.DictCursor)
-            sql = 'getUserByUname'
-            cursor = dbConnection.cursor()
-            sqlArgs = (username,)
-            cursor.callproc(sql,sqlArgs)
-            row = cursor.fetchone()
-            if row is None:
-                abort(404)
-        except:
-            abort(500)
-        finally:
-            cursor.close()
-            dbConnection.close()
-        return make_response(jsonify({"user": row}), 200)
+        #GET: get info about user
+        #UPDATE: Update User info
+        def get(self, username):
+                try:
+                        dbConnection = pymysql.connect(settings.DB_HOST,
+                                settings.DB_USER,
+                                settings.DB_PASSWD,
+                                settings.DB_DATABASE,
+                                charset='utf8mb4',
+                                cursorclass= pymysql.cursors.DictCursor)
+                        sql = 'getUserByUname'
+                        cursor = dbConnection.cursor()
+                        sqlArgs = (username,)
+                        cursor.callproc(sql,sqlArgs)
+                        row = cursor.fetchone()
+                        if row is None:
+                                abort(404)
+                except:
+                        abort(500)
+                finally:
+                        cursor.close()
+                        dbConnection.close()
+                return make_response(jsonify({"user": row}), 200)
         
     
 
-    def update(self, userId):
-        return
+        def update(self, userId):
+                return
 
 api.add_resource(User, '/users/<int:userId>')
 
 class Images(Resource):
-    def get(self, userId):
-        #Retrieving parameters
-        url = request.url
-        imageId = None
-        if "imageId" in url:
-            imageId = request.args.get("imageId")
-        try:
-            dbConnection = pymysql.connect(
-				settings.DB_HOST,
-				settings.DB_USER,
-				settings.DB_PASSWD,
-				settings.DB_DATABASE,
-				charset='utf8mb4',
-				cursorclass= pymysql.cursors.DictCursor)
-            if imageId is None:
-                #Args: owner
-                sql = 'getImages'
-                sqlArgs = (userId)
-            else:
-                #Args: owner, imageId
-                sql = 'getImageById'
-                sqlArgs = (userId, imageId)
+        def get(self, userId):
+                #Retrieving parameters
+                url = request.url
+                imageId = None
+                if "imageId" in url:
+                        imageId = request.args.get("imageId")
+                try:
+                        dbConnection = pymysql.connect(settings.DB_HOST,
+                                settings.DB_USER,
+                                settings.DB_PASSWD,
+                                settings.DB_DATABASE,
+                                charset='utf8mb4',
+                                cursorclass= pymysql.cursors.DictCursor)
+                        if imageId is None:
+                                sql = 'getImages'
+                                sqlArgs = (userId)
+                        else:
+                                #Args: owner, imageId
+                                sql = 'getImageById'
+                                sqlArgs = (userId, imageId)
 
-            cursor = dbConnection.cursor()
-            cursor.callproc(sql,sqlArgs)
-            rows = cursor.fetchall()
-            for row in rows:
-                pass
-        except:
-            abort(500)
-        return 'HEy'
+                        cursor = dbConnection.cursor()
+                        cursor.callproc(sql,sqlArgs)
+                        rows = cursor.fetchall()
+                        for row in rows:
+                                pass
+                except:
+                        abort(500)
+                return 'HEy'
     
-    def post(self):
-        if not request.json:
-            abort(400)#bad request
+        def post(self):
+                if not request.json:
+                        abort(400)#bad request
         
-        #Parse Json
-        parser = reqparse.RequestParser()
-        #try:
-        return "Hey"
+                #Parse Json
+                parser = reqparse.RequestParser()
+                #try:
+                return "Hey"
             
 
 api.add_resource(Images, '/users/<int:userId>/images')
+
+if __name__ == "__main__":
+   	app.run(host=settings.APP_HOST, port=settings.APP_PORT, debug=settings.APP_DEBUG)
